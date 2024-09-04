ef969e08cdad9ca092240da5d35dcc4c35babb24
Bensaxxy <benjaminsegun38@gmail.com>
2024-09-04 15:01:23 +0100

    push
    pushCodes

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navbarMenu = document.getElementById("navbar-menu");
+
+  menuIcon.addEventListener("click", function () {
+    if (
+      navbarMenu.style.display === "none" ||
+      navbarMenu.style.display === ""
+    ) {
+      navbarMenu.style.display = "flex";
+    } else {
+      navbarMenu.style.display = "none";
+    }
+  });
+});
+s;
